19fee0d331efabf2d9bab6f58c19c269833c82da
Leon30 <portal.fernando@hotmail.com>
2016-07-21 11:11:41 -0500

    Update SoccerForecaster.java

--- soccerForecaster/src/models/SoccerForecaster.java
+++ soccerForecaster/src/models/SoccerForecaster.java
@@ -5,20 +5,28 @@ import java.util.Scanner;
 public class SoccerForecaster {
 
     public static final int MAX_TEAMS = 20;
-    private final Scanner scanner;
+    public static final int MAX_MATCHS = 20;
     private Match[] matchList;
-    private Team[] teams;
+    private Team[] teamList;
 
     public SoccerForecaster() {
-        scanner = new Scanner(System.in);
-        teams = new Team[MAX_TEAMS];
-        teams[0] = new Team("");
+        matchList = new Match[MAX_MATCHS];
+        teamList = new Team[MAX_TEAMS];
+        initTeams();
     }
     
     public void registerMatch(Match m){
         for (int i = 0; i < matchList.length; i++) {
             if (matchList[i] == null) {
                 matchList[i]= m;
+            }
+        }
+    }
+    
+    public void registerTeam(Team t){
+        for (int i = 0; i < teamList.length; i++) {
+            if (teamList[i] == null) {
+                teamList[i]= t;
             }
         }
     }
@@ -35,6 +43,10 @@ public class SoccerForecaster {
     	        return m.getTeams()[1];
     	    }
     	}
+    }
+    
+    public void getProbalility(){
+        
     }
 
     public static void main(String[] args) {
